82be0a0235b9978abadafbcf70cbb12ead278b0a
Andy Tran <ajtran@umich.edu>
2015-06-26 02:32:38 -0400

    not a lot going on, attempted login function

--- blog/urls.py
+++ blog/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^login/', include('login.urls', namespace='login')),
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ login/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404, render
+from django.template import RequestContext, loader
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from .models import User
+
+def index(request):
+	return render(request, 'login/index.html')
+
+def attempt(request):
+	return render(request, 'login/yes.html')

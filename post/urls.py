864e39aedb1f056d638ced83ba8b506fdecb6991
Andy Tran <ajtran@umich.edu>
2015-07-14 23:01:00 -0400

    'attempted feature for new posts'

--- post/urls.py
+++ post/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url, patterns
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<user_id>[0-9]+)/$', views.what, name='what'),
-    url(r'^(?P<user_id>[0-9]+)/test/$', views.what, name='what'),
-
+    #url(r'^(?P<user_id>[0-9]+)/$', views.main, name='main'),
+    url(r'^$', views.test, name='test'),
+    url(r'^new/$', views.new, name='new'),
+    url(r'^submit/$', views.submit, name='submit'),
     """url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),"""
--- post/views.py
+++ post/views.py
@@ -7,10 +7,20 @@ from django.views import generic
 from login.models import User
 #from login.views import go, 
 
-def what(request, user_id):
+def main(request, user_id):
 	u = get_object_or_404(User, pk=user_id)
 	return render(request, 'post/index.html', {'user' : u})
 
 def test(request):
 	u = get_object_or_404(User, pk=1)
+	return render(request, 'post/index.html', {'user' : u})
+	#return render(request, 'login/index.html')
+
+def new(request, user_id):
+	u = get_object_or_404(User, pk=user_id)
+	return render(request, 'post/new.html', {'user' : u})
+
+def submit(request, user_id):
+	u = get_object_or_404(User, pk=user_id)
+	new_post = Post(username=u.username, subject=request.POST['subject'],text=request.POST['body'])
 	return render(request, 'post/index.html', {'user' : u})

d74a450d2944eb3ef11e8794b96f1e213ab5d43e
Andy Tran <ajtran@umich.edu>
2015-07-18 22:25:42 -0400

    'still working on login.  form making taking a bit longer'

--- login/urls.py
+++ login/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<user_id>[0-9]+)/go/$', views.go, name='go'),
+    url(r'^attempt/$', views.attempt, name='attempt'),
 ]
--- login/views.py
+++ login/views.py
@@ -11,10 +11,52 @@ def index(request):
 	user_list = User.objects.all()
 	log = LoginForm()
 	reg = RegForm()
+	badlog=''
+	badreg=''
 	return render(request, 'login/index.html', {'log' : log, 'reg' : reg}) 
 
 def go(request, user_id):
 	u = get_object_or_404(User, pk=user_id)
 	#return HttpResponseRedirect(reverse('post:test'))
 	return render(request, 'login/attempt.html', {'name' : u.username})
+def attempt(request):
+	if request.method == 'GET':
+		attempt = LoginForm(request.GET)
+		if attempt.is_valid():
+			try:
+				test = User.objects.get(username=attempt.cleaned_data['user_name'])
+			except User.DoesNotExist:
+				badlog = "user doesn't exist"
+				#return render(request, 'login/index.html', {'badlog' : badlog})
+				#return redirect('login:index')
+				raise ValidationError()
+			return redirect(reverse('post:main', args=(test.username,)))
+		else:
+			badlog = "Not all fields used"
+			return redirect('login:index', badlog)
+
+	if request.method == 'POST':
+		attempt = RegForm(request.POST)
+		if attempt.is_valid():
+			try:
+				test = User.objects.get(user_name.cleaned_data['username'])
+			except User.DoesNotExist:
+				#check passwords match
+				if attempt.cleaned_data['password'] != attempt.cleand_data['password2']:
+					badreg = "passwords do not match"
+					return render(request, 'login/index.html', {'badreg' : badreg})
+				else:
+					new_user = User(username=attempt.cleand_data['username'],password=attempt.cleand_data['password'])
+					new_user.save()
+					#return HttpResponseRedirect(reverse('login:register'))
+
+			badreg = "username already taken"
+			return render(request, 'login/index.html', {'badreg' : badreg})
+		else:
+			badreg = "Not all fields used"
+			return render(request, 'login/index.html', {'badreg' : badreg})
+#def register(request):
+
+
+
 	#return render(request, '/post/', {})

5315c9e7cf2f654264b909b84dd1e00d84e3af01
Andy Tran <ajtran@umich.edu>
2015-07-22 02:07:03 -0400

    'tried to add style + login and it did not work'

--- login/views.py
+++ login/views.py
@@ -59,6 +59,8 @@ def attempt(request):
 def done(request, username):
 	u = get_object_or_404(User, username=username)
 	auth.logout(request)
+	u.is_authenticated = False
+	u.save()
 	return redirect(reverse('login:index'))
 
 
